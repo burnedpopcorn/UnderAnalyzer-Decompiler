d610f0811e362667fd59cc41adca95d2d6869170
burnedpopcorn <164114576+burnedpopcorn@users.noreply.github.com>
2024-12-07 22:35:43 -0800

    finalized

--- UndertaleModLib/obj/Release/net6.0/UndertaleModLib.AssemblyInfo.cs
+++ UndertaleModLib/obj/Release/net6.0/UndertaleModLib.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright ©  2018-2024, licensed under GPLv3")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.6.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dcacffb9ba5ef9eedbfae9bd9938c1c550e74c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddecc268511a8d29c7f50e5d3beae12cb7fe25f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UndertaleModLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UndertaleModLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.6.0.0")]
