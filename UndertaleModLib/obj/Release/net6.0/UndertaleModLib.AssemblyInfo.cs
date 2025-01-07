a5ba4abc07258edfd531ee539af39ec135a56b91
burnedpopcorn <164114576+burnedpopcorn@users.noreply.github.com>
2025-01-06 18:36:06 -0800

    holy crack its basically done

--- UndertaleModLib/obj/Release/net6.0/UndertaleModLib.AssemblyInfo.cs
+++ UndertaleModLib/obj/Release/net6.0/UndertaleModLib.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright ©  2018-2024, licensed under GPLv3")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.6.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e1ab9468e87caae6e0824b65a0b2c94eabb3ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f0324b8ac078271c4bb4563f873aff3e9d763a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UndertaleModLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UndertaleModLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.6.0.0")]
